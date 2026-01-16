c0ea4024e34f517e400b93dab249b54822cff2a9
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-16 22:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //your JS code here. If required.
-const bookList = document.getElementById("book-list")
-const main = document.getElementsByClassName("main")[0];
+const main = document.querySelector(".main");
 const submit = document.getElementById("submit")
 const cancel = document.getElementById("cancel")
 const title = document.getElementById("titleVV")
-const auther = document.getElementById("autherVV")
+const author = document.getElementById("authorVV")
 const isbn = document.getElementById("isbnVV")
 const titleI = document.getElementById("title")
-const autherI = document.getElementById("auther")
+const authorI = document.getElementById("author")
 const isbnI = document.getElementById("isbn")
 const thead = document.getElementById("thead")
 
-addbook.addEventListener("click",()=>{
+submit.addEventListener("click",()=>{
 	bookList.style.display= ""
-	title.value = titleVV.value
-	auther.value = autherVV.value
-	isbn.value = isbnVV.value
+	titleVV.textContent = title.value
+	authorVV.textContent = author.value
+	isbnVV.textContent = isbn.value
 })
 cancel.addEventListener("click",()=>{
 	thead.style.display = "none";
