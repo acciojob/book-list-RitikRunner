734bcfd7da3ad0eb3056e3c9eab41767e71e27fd
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-16 22:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const bookList = document.getElementById("book-list")
+const main = document.getElementsByClassName("main")[0];
+const addbook = document.getElementById("addBook")
+const cancel = document.getElementById("cancel")
+const title = document.getElementById("title")
+const auther = document.getElementById("auther")
+const isbn = document.getElementById("isbn")
+const titleI = document.getElementById("titleI")
+const autherI = document.getElementById("autherI")
+const isbnI = document.getElementById("isbnI")
+const thead = document.getElementById("thead")
+
+addbook.addEventListener("click",()=>{
+	bookList.style.display= ""
+	titleI.value = title.value
+	autherI.value = auther.value
+	isbnI.value = isbn.value
+})
+cancel.addEventListener("click",()=>{
+	thead.style.display = "none";
+})
+
+
+
+
+
+
+
+
+
+
