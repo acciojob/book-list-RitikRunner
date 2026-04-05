4d6fb67e20d863755361cb68804ed44976e51368
Ritik Sharma <ritiksharma10508@gmail.com>
2026-04-05 14:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 //your JS code here. If required.
 const main = document.querySelector(".main");
 const submit = document.getElementById("submit")
-const cancel = document.getElementById("cancel")
-const title = document.getElementById("titleVV")
-const author = document.getElementById("authorVV")
-const isbn = document.getElementById("isbnVV")
 const titleI = document.getElementById("title")
 const authorI = document.getElementById("author")
 const isbnI = document.getElementById("isbn")
 const thead = document.getElementById("thead")
 const bookList = document.getElementById("book-list");
 
+
 submit.addEventListener("click",()=>{
-	bookList.style.display= "table";
-	title.textContent = titleI.value
-	author.textContent = authorI.value
-	isbn.textContent = isbnI.value
+
+	if(titleI.value === "" || authorI.value ==="" || isbnI.value ===""){
+		alert("Fill all feilds")
+		return;
+	}
+	const tr = document.createElement("tr")
+	tr.innerHTML =`
+   <td>${titleI.value}</td>
+   <td>${authorI.value}</td>
+   <td>${isbnI.value}</td>
+   <td><button class="delete btn btn-danger">X</button></td>
+`
+thead.appendChild(tr);
+	titleI.value = "";
+	authorI.value = "";
+	isbnI.value = ""
 })
-cancel.addEventListener("click",()=>{
-	thead.style.display = "none";
+thead.addEventListener("click",(e)=>{
+	if(e.target.classList.contains("delete")){
+		e.target.parentElement.parentElement.remove();
+	}
 })
+
+
+
+
+
+
 
 
 
